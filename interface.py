ca2f17f36e459f61e324d040edd5b15f7680f962
rabab <rabab.bouziane@etudiant.univ-lille1.fr>
2013-01-21 15:10:09 +0100

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,43 +1,51 @@
+
 import Tkinter
-class simpleapp_tk(Tkinter.Tk):
+import Tix 
+class simpleapp_tk(Tix.Tk):
     def __init__(self,parent):
-        Tkinter.Tk.__init__(self,parent)
+        Tix.Tk.__init__(self,parent)
         self.parent=parent
         self.initialize()
 
-    def initialize self:
-        (self).grid()
-        self .entryVariable= Tkinter.StringVar()
-        self.entry1=Tkinter.Entry(self, textvariable=self.entryVariable)
-        self.entry2=Tkinter.Entry(self)
-        self.entry1.grid(column=1, row=0, sticky='EW')
-        self.entry2.grid(column=1, row=1, sticky='EW')
-        self.entry1.bind("<Return>", self.OnPressEnter)
-        self.entryVariable.set(u"Enter text here")
+    def initialize (self):
+        #root = Tix.Tk()
+        self.grid()
+        varcombo = Tix.StringVar() 
+        text= Tix.Label(self, text='target')
+        text.grid(column=0, row=0, sticky='EW')
+        text.pack()
+        bal= Tix.Balloon()
+        self.enabled= Tix.IntVar()
+        self.checkbutton1 = Tix.Checkbutton(self,text = "WSN430", variable=self.enabled,command=self.OnCheckBoxClick)
+        self.checkbutton1.pack()
+        self.checkbutton2 = Tix.Checkbutton(self,text = "WSN430", variable=self.enabled,command=self.OnCheckBoxClick)
+        self.checkbutton2.pack()
+        self.checkbutton3 = Tix.Checkbutton(self,text = "MicaZ", variable=self.enabled,command=self.OnCheckBoxClick)
+        self.checkbutton3.pack()
+        self.checkbutton4 = Tix.Checkbutton(self,text = "Funcard7", variable=self.enabled,command=self.OnCheckBoxClick)
+        self.checkbutton4.pack()
+        self.checkbutton5 = Tix.Checkbutton(self,text = "GBA", variable=self.enabled,command=self.OnCheckBoxClick)
+        self.checkbutton5.pack()
+        self.checkbutton6 = Tix.Checkbutton(self,text = "MBED", variable=self.enabled,command=self.OnCheckBoxClick)
+        self.checkbutton6.pack()
+        self.checkbutton7 = Tix.Checkbutton(self,text = "Linux", variable=self.enabled,command=self.OnCheckBoxClick)
+        self.checkbutton7.pack()
+        self.checkbutton8 = Tix.Checkbutton(self,text = "Skeleton", variable=self.enabled,command=self.OnCheckBoxClick)
+        self.checkbutton8.pack()
+        combo = Tix.ComboBox(self, editable=1, dropdown=1, variable=varcombo, command = self.Affiche)
+        combo.entry.config(state='readonly')  
+        combo.insert(0, 'NT') 
+        combo.insert(1, 'Linux')
+        bal.bind_widget(combo, msg='target')
+        combo.pack()
+        self.mainloop()
 
-        button= Tkinter.Button(self, text=u"click me !", command=self.OnButtonClick)
-        button.grid(column=2, row=0)
+    def OnCheckBoxClick(self):
+        iTemp = self.enabled.get()
+        print "iTemp %s"%iTemp
 
-        self.labelVariable= Tkinter.StringVar()
-        label= Tkinter.Label(self,textvariable=self.labelVariable, text="here", anchor="w", fg="white", bg="blue")
-        self.grid_columnconfigure(0, weight=1)
-        label.grid(column=0, row=1, columnspan=2, sticky='EW')
-        self.resizable(True, False)
-        self.update()
-        self.geometry(self.geometry())
-        self.entry1.focus_set()
-        self.entry1.selection_range(0, Tkinter.END)
-
-    def OnButtonClick(self):
-        self.labelVariable.set("you clicked the boutton !")
-        self.entry1.focus_set()
-        self.entry1.selection_range(0, Tkinter.END)
-
-    def OnPressEnter(self,event):
-        self.labelVariable.set("you pressed enter !")
-        self.entry1.focus_set()
-        self.entry1.selection_range(0, Tkinter.END)
-
+    def Affiche(self,evt):
+        print varcombo.get()
 if __name__== "__main__":
     app=simpleapp_tk(None)
     app.title('my application')
